9c4fe747b011c4b22baa83775a1f8d75b5d11972
Morgan Newman <mpnewman95@gmail.com>
2019-03-01 11:16:45 -0800

    Updates CTA button

--- src/components/common/CTAButton.js
+++ src/components/common/CTAButton.js
@@ -3,9 +3,5 @@ import React from 'react';
 import { FaPhone as PhoneIcon } from 'react-icons/fa';
 
 export default function CTA() {
-  return (
-    <button className="cta-button">
-      <PhoneIcon className="cta-phone" /> Call Now
-    </button>
-  );
+  return <button className="cta-button">Schedule a Call</button>;
 }
