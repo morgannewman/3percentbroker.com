0323136124b78e184ca24530da0e9cdda555e470
Morgan Newman <mpnewman95@gmail.com>
2019-03-01 11:47:59 -0800

    Implements Nav elements and styling

--- src/components/common/CTAButton.js
+++ src/components/common/CTAButton.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { FaPhone as PhoneIcon } from 'react-icons/fa';
-
 export default function CTA() {
   return <button className="cta-button">Schedule a Call</button>;
 }
--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
+import { MdSearch as SearchIcon } from 'react-icons/md';
+
 export default class Nav extends React.Component {
   render() {
     const { className } = this.props;
     return (
       <header className={`nav ${className || ''}`}>
-        <img
-          className="nav-logo"
-          src={require('../../assets/white-logo.png')}
-          alt=""
-        />
+        <a href="/">
+          <img
+            className="nav-logo"
+            src={require('../../assets/white-logo.png')}
+            alt="3PercentBroker.com Logo"
+          />
+        </a>
         <nav className="nav-nav">
-          <a href="#Pricing">Pricing</a>
+          <a className="nav-link" href="#Pricing">
+            Pricing
+          </a>
+          <button className="nav-link nav-link_search">
+            <span className="nav-link_search-text">Property Search</span>{' '}
+            <SearchIcon className="nav-link_search-icon" />
+          </button>
+          <button className="nav-link">209-765-7264</button>
         </nav>
       </header>
     );
